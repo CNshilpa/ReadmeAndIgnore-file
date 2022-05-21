0a1f1e07543ae0d92dcc9662e4374f27c8dc2484
CNshilpa <shilpa.cnv@gmail.com>
2022-05-21 20:46:53 +0530

    Added : msg,Readme and GitIgnore

---
+++ Readme.md
@@ -0,0 +1,1 @@
+Readme having current program information
---
+++ src/ReadmeIgnoreFile.java
@@ -0,0 +1,9 @@
+
+public class ReadmeIgnoreFile {
+
+	public static void main(String[] args) {
+		System.out.println("Avoid Readme and Gitignore files");
+
+	}
+
+}
